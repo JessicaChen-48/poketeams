417dd58d8be4c36cc34e9560b229c5e70a243ec1
Jessica <jessica@jessicas-mbp.lan>
2021-05-15 15:50:01 -0700

    add type icon images for background

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Background from "./Background"
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Background />
   </React.StrictMode>,
   document.getElementById('root')
 );
